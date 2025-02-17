e22022c91f476f55aa863a62e4c53fff7a5f4ebd
akouba <akoubandri@gmail.com>
2025-02-17 18:03:55 +0000

    ajout de membres du conseil

---
+++ src/main/java/CNPS/DONNEES_COMPTABLES/jpa/entity/Board_members.java
@@ -0,0 +1,42 @@
+package CNPS.DONNEES_COMPTABLES.jpa.entity
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "board_members")
+public class Board_members {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="id_members", nullable = false )
+    private UUID id_members;
+
+    @Column (name ="name_members")
+    private String nameMembers;
+
+    @Column(name = "first_name_members")
+    private String firstNameMembers;
+
+    @Column(name = "structure_members")
+    private String structureMembers;
+
+    @Column(name = "percentage_members")
+    private String percentageMembers;
+
+    @Column(name = "address_members")
+    private String addressMembers;
+
+    @Column(name = "nationality_members")
+    private String nationalityMembers;
+
+    @ManyToOne
+    private Company company;
+}
